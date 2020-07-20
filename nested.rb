ee172f65c31fd7be448cb0766006d17a3640b529
Augusto Ibarrola <augusto.t.ibarrola@gmail.com>
2020-07-20 19:59:54 +0000

    Automatically backed up by Learn

--- nested.rb
+++ nested.rb
@@ -94,9 +94,8 @@ def changing_alan
         :known_for => "Unix",
         :languages => ["C"]
       }
-    },
+    }
 		programmer_hash[:alan_kay][:known_for] = "GUI"
-		programmer_hash
 end
 
 def adding_to_dennis
